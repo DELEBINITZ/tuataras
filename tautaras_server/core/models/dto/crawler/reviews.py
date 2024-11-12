1c02744b9beccbd9eebb3a6c7abda1b1bee796be
akshayTupe00 <akshay.tupe@kirana.club>
2024-11-12 23:29:19 +0530

    added dto for reviews response

--- tautaras_server/core/models/dto/crawler/reviews.py
+++ tautaras_server/core/models/dto/crawler/reviews.py
@@ -1,5 +1,28 @@
 from pydantic import BaseModel, Field
-
+from typing import List, Optional
 
 class ExtractReviewRequest(BaseModel):
     url: str = Field(...)
+
+class ReviewerDetails(BaseModel):
+    location: Optional[str] = None
+
+class ReviewDTO(BaseModel):
+    review_id: str
+    product_name: str
+    site_name: str
+    rating: float
+    title: str
+    description: str
+    reviewer: str
+    reviewer_location: Optional[str] = None
+    indexed_at: str
+    updated_at: str
+
+class PaginatedResponse(BaseModel):
+    status: str
+    page: int
+    page_size: int
+    total_results: int
+    total_pages: int
+    reviews: List[ReviewDTO]
