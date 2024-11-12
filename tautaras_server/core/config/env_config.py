d483936cd9ad1de5f288a133635a98d3df2bf28c
akshayTupe00 <akshay.tupe@kirana.club>
2024-11-12 11:31:09 +0530

    added congiguration files

--- tautaras_server/core/config/env_config.py
+++ tautaras_server/core/config/env_config.py
@@ -2,8 +2,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-dotenv_path = Path(__file__).resolve().parent / ".." / ".env"
+dotenv_path = Path(__file__).resolve().parent / ".." / ".." / ".env"
 
 load_dotenv(dotenv_path)
 
 sttgs = os.environ
+
